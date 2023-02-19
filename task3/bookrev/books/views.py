dd186f9ad1300e2068ab779bdfb6f70cbf9d5962
Yaroslavaaaa <105859725+Yaroslavaaaa@users.noreply.github.com>
2023-02-19 18:56:20 +0600

    task3
    task 3

---
+++ task3/bookrev/books/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+from django.template import RequestContext
+
+
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse("Страница книг")
+    return render(request, "books/index.html", {'title': 'Главная страница'})
+def book(request, bid):
+    return  render(request, "books/index.html")
+    # return HttpResponse(f"Страница книг, {bid}")
+
+def error404(request, exeption):
+    return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")
+
+def error500(request):
+    return HttpResponseNotFound("<h1>Ошибка сервера</h1>")
+
+def error400(request, exeption):
+    return HttpResponseNotFound("<h1>Некорректный запрос</h1>")
+
+def error403(request, exeption):
+    return HttpResponseNotFound("<h1>Доступ запрещен</h1>")
